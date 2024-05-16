bd2ec5ab5444c130186e8e43d304ed27a33841d1
Mike Teehan <mike.teehan@gmail.com>
2024-05-16 18:24:53 -0400

    Regenerated for Qt6

--- ui_dafgen.py
+++ ui_dafgen.py
@@ -1,79 +1,78 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'dafgen.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
+################################################################################
+## Form generated from reading UI file 'dafgen.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QLabel, QPlainTextEdit,
+    QPushButton, QSizePolicy, QSlider, QWidget)
 
 class Ui_DAFGen(object):
     def setupUi(self, DAFGen):
-        DAFGen.setObjectName("DAFGen")
+        if not DAFGen.objectName():
+            DAFGen.setObjectName(u"DAFGen")
         DAFGen.resize(451, 95)
-        DAFGen.setMinimumSize(QtCore.QSize(451, 95))
-        DAFGen.setMaximumSize(QtCore.QSize(451, 95))
-        font = QtGui.QFont()
+        DAFGen.setMinimumSize(QSize(451, 95))
+        DAFGen.setMaximumSize(QSize(451, 95))
+        font = QFont()
         font.setPointSize(11)
         DAFGen.setFont(font)
-        self.delayLabel = QtWidgets.QLabel(DAFGen)
-        self.delayLabel.setGeometry(QtCore.QRect(20, 10, 61, 21))
-        font = QtGui.QFont()
-        font.setPointSize(11)
+        self.delayLabel = QLabel(DAFGen)
+        self.delayLabel.setObjectName(u"delayLabel")
+        self.delayLabel.setGeometry(QRect(20, 10, 61, 21))
         self.delayLabel.setFont(font)
-        self.delayLabel.setObjectName("delayLabel")
-        self.actualDelayLabel = QtWidgets.QLabel(DAFGen)
-        self.actualDelayLabel.setGeometry(QtCore.QRect(20, 50, 91, 31))
-        font = QtGui.QFont()
-        font.setPointSize(11)
+        self.actualDelayLabel = QLabel(DAFGen)
+        self.actualDelayLabel.setObjectName(u"actualDelayLabel")
+        self.actualDelayLabel.setGeometry(QRect(20, 50, 91, 31))
         self.actualDelayLabel.setFont(font)
-        self.actualDelayLabel.setObjectName("actualDelayLabel")
-        self.startButton = QtWidgets.QPushButton(DAFGen)
-        self.startButton.setGeometry(QtCore.QRect(200, 50, 71, 31))
-        self.startButton.setObjectName("startButton")
-        self.stopButton = QtWidgets.QPushButton(DAFGen)
-        self.stopButton.setGeometry(QtCore.QRect(280, 50, 71, 31))
-        self.stopButton.setObjectName("stopButton")
-        self.quitButton = QtWidgets.QPushButton(DAFGen)
-        self.quitButton.setGeometry(QtCore.QRect(360, 50, 71, 31))
-        self.quitButton.setObjectName("quitButton")
-        self.delaySlider = QtWidgets.QSlider(DAFGen)
-        self.delaySlider.setGeometry(QtCore.QRect(120, 10, 241, 21))
+        self.startButton = QPushButton(DAFGen)
+        self.startButton.setObjectName(u"startButton")
+        self.startButton.setGeometry(QRect(200, 50, 71, 31))
+        self.stopButton = QPushButton(DAFGen)
+        self.stopButton.setObjectName(u"stopButton")
+        self.stopButton.setGeometry(QRect(280, 50, 71, 31))
+        self.quitButton = QPushButton(DAFGen)
+        self.quitButton.setObjectName(u"quitButton")
+        self.quitButton.setGeometry(QRect(360, 50, 71, 31))
+        self.delaySlider = QSlider(DAFGen)
+        self.delaySlider.setObjectName(u"delaySlider")
+        self.delaySlider.setGeometry(QRect(120, 10, 241, 21))
         self.delaySlider.setMinimum(50)
         self.delaySlider.setMaximum(200)
-        self.delaySlider.setOrientation(QtCore.Qt.Horizontal)
-        self.delaySlider.setObjectName("delaySlider")
-        self.delayEdit = QtWidgets.QPlainTextEdit(DAFGen)
-        self.delayEdit.setGeometry(QtCore.QRect(370, 10, 61, 31))
-        self.delayEdit.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.delaySlider.setOrientation(Qt.Horizontal)
+        self.delayEdit = QPlainTextEdit(DAFGen)
+        self.delayEdit.setObjectName(u"delayEdit")
+        self.delayEdit.setGeometry(QRect(370, 10, 61, 31))
+        self.delayEdit.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.delayEdit.setReadOnly(True)
-        self.delayEdit.setObjectName("delayEdit")
-        self.actualDelayEdit = QtWidgets.QPlainTextEdit(DAFGen)
-        self.actualDelayEdit.setGeometry(QtCore.QRect(120, 50, 61, 31))
-        self.actualDelayEdit.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.actualDelayEdit = QPlainTextEdit(DAFGen)
+        self.actualDelayEdit.setObjectName(u"actualDelayEdit")
+        self.actualDelayEdit.setGeometry(QRect(120, 50, 61, 31))
+        self.actualDelayEdit.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.actualDelayEdit.setReadOnly(True)
-        self.actualDelayEdit.setObjectName("actualDelayEdit")
 
         self.retranslateUi(DAFGen)
-        QtCore.QMetaObject.connectSlotsByName(DAFGen)
+
+        QMetaObject.connectSlotsByName(DAFGen)
+    # setupUi
 
     def retranslateUi(self, DAFGen):
-        _translate = QtCore.QCoreApplication.translate
-        DAFGen.setWindowTitle(_translate("DAFGen", "DAF Gen"))
-        self.delayLabel.setText(_translate("DAFGen", "Delay"))
-        self.actualDelayLabel.setText(_translate("DAFGen", "Actual Delay"))
-        self.startButton.setText(_translate("DAFGen", "Start"))
-        self.stopButton.setText(_translate("DAFGen", "Stop"))
-        self.quitButton.setText(_translate("DAFGen", "Quit"))
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    DAFGen = QtWidgets.QDialog()
-    ui = Ui_DAFGen()
-    ui.setupUi(DAFGen)
-    DAFGen.show()
-    sys.exit(app.exec_())
+        DAFGen.setWindowTitle(QCoreApplication.translate("DAFGen", u"DAF Gen", None))
+        self.delayLabel.setText(QCoreApplication.translate("DAFGen", u"Delay", None))
+        self.actualDelayLabel.setText(QCoreApplication.translate("DAFGen", u"Actual Delay", None))
+        self.startButton.setText(QCoreApplication.translate("DAFGen", u"Start", None))
+        self.stopButton.setText(QCoreApplication.translate("DAFGen", u"Stop", None))
+        self.quitButton.setText(QCoreApplication.translate("DAFGen", u"Quit", None))
+    # retranslateUi
 
